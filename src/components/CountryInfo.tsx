46d58dfcbcbf236ed6ac0348a9bf81e9c5686c00
Marley Anthony <62618323+marleyanthony@users.noreply.github.com>
2021-01-28 13:23:47 -0800

    added comma spacing for countries

--- src/components/CountryInfo.tsx
+++ src/components/CountryInfo.tsx
@@ -1,11 +1,30 @@
+import { ThemeContext } from '../context/ThemeContext';
+import { useContext, useEffect } from 'react';
+import backArrow from '../assets/icons/back-arrow-dark.svg';
+import backArrowLight from '../assets/icons/back-arrow.svg'
+
 const CountryInfo = (props: any) => {
-   const { flag, name, nativeName, population, region, subregion, capital, currencies, languages } = props.location.state;
-   console.log(languages)
+   useEffect(() => {
+      window.scrollTo(0, 0);
+   }, []);
+
+   const { flag, name, nativeName, population, region, subregion, capital, currencies, languages, borders } = props.location.state;
+
+   const theme = useContext(ThemeContext);
 
    return (
-      <section className="country-info">
+      <section className={theme.theme === 'light' ? 'country country--light' : 'country'}>
+         {/* <Link to="/" className="country__link"> */}
+         <a href="/" className="country__back-button">
+            {
+               theme.theme === 'light'
+                  ? <img src={backArrowLight} alt="back arrow" className="country__back-arrow" />
+                  : <img src={backArrow} alt="back arrow" className="country__back-arrow" />
+            } Back
+            </a>
+         {/* </Link> */}
          <div className="country__flag">
-            <img src={flag} alt="country flag" />
+            <img src={flag} alt="country flag" className="country__img" />
          </div>
          <div className="country__info">
             <h1 className="country__name">
@@ -32,8 +51,32 @@ const CountryInfo = (props: any) => {
                      <span className="country__label-bold">Currency:</span> {currencies[0].code}
                   </p>
                   <p className="country__label">
-                     <span className="country__label-bold">Languages:</span> {languages.map((language: any, index: number) => <p key={index}>{language.name}</p>)}
+                     <span className="country__label-bold">Languages:</span> {
+                        languages.map((language: any, index: number) => {
+                           return (
+                              <div className="country__languages-wrapper" key={index}>
+                                 <p>
+                                    {(index ? ', ' : '') + language.name}
+                                 </p>
+                              </div>
+                           )
+                        })
+                     }
                   </p>
+               </div>
+            </div>
+            <div className="country__border-wrapper">
+               <h1 className="country__border-heading">
+                  Border Countries:
+               </h1>
+               <div className="country__border-country-wrapper">
+                  {
+                     borders.map((border: any, index: number) => {
+                        return (
+                           <p className="country__border" key={index}>{border}</p>
+                        )
+                     })
+                  }
                </div>
             </div>
          </div>
